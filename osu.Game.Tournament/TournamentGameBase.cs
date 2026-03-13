3db8a27e8b4c2e09a536e38bd2adbbd30d171865
cdwcgt <cdwcgt@cdwcgt.top>
2026-03-13 16:24:40 +0800

    什么意思呢

--- osu.Game.Tournament/TournamentGameBase.cs
+++ osu.Game.Tournament/TournamentGameBase.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions;
 using osu.Framework.Extensions.ObjectExtensions;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Textures;
@@ -14,7 +15,9 @@ using osu.Framework.Input;
 using osu.Framework.IO.Stores;
 using osu.Framework.Logging;
 using osu.Framework.Platform;
+using osu.Framework.Utils;
 using osu.Game.Beatmaps.Legacy;
+using osu.Game.Configuration;
 using osu.Game.Database;
 using osu.Game.Graphics;
 using osu.Game.Online;
@@ -63,8 +66,23 @@ namespace osu.Game.Tournament
             base.SetHost(host);
 
             if (host.Window != null)
-                host.Window.Title = $"HSC - Tournamnet 20w14∞ [别看这个，快去看选手操作！]";
+                host.Window.Title = $"HSC - Tournamnet 20w14∞ [{randomTitle}] {versionSniffer} 版本:{Version}";
         }
+
+        private static readonly string[] titles =
+        {
+            "这窗口值一个 S rank",
+            "别看这个，快去看选手操作！",
+            "正在检测选手的屏幕压力...检测结果：炸了",
+            "鼠标灵敏度已重置为：随缘",
+            "请按 F1 寻求如何不气死自己的方法",
+            "加载中... (其实根本没在加载)",
+            "👌👌👌👌👌👌👌",
+        };
+
+        private string randomTitle => titles[RNG.Next(0, titles.Length)];
+
+        private string versionSniffer => ReleaseStream.General.GetDescription();
 
         private TournamentSpriteText initialisationText = null!;
 
